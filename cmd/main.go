93f18ed5b8ae9b7f3950ac3f61bf82422a432313
Achmad An'im <animfahmy@yahoo.co.id>
2022-08-02 10:05:02 +0700

    endpoint /v1/companies

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"net/http"
+	"github.com/labstack/echo/v4"
+	"log"
+	"time"
+	"encoding/json"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", HandlerMain)
+	version := "/v1"
+	e.GET(version + "/companies", HandlerCompanies)
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func HandlerMain(echCtx echo.Context) error {
+	return echCtx.String(http.StatusOK, "Hello, World!")
+}
+
+type Companies struct{
+	id uint64
+	name string
+	description string
+	created_at time.Time
+	updated_at time.Time
+}
+
+func HandlerCompanies(echCtx echo.Context) error {
+	existingCompanies := []Companies{
+		{
+			id: 1,
+			name: "PT Majoo Teknologi Indonesia",
+			description: "Aplikasi Wirausaha",
+		},
+	}
+	jsonCompanies, err := json.Marshal(existingCompanies)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return echCtx.JSON(http.StatusOK, jsonCompanies)
+}
